c8c4bc77ec154f808a0dfad8364e078bc66c6d3b
Alaa-1 <aladdinabidi@gmail.com>
2022-07-12 10:16:33 +0100

    Create button-clicker.js

---
+++ basic-js/button-clicker/button-clicker.js
@@ -0,0 +1,11 @@
+function login(element) {
+    element.innerText = "Logout";
+}
+
+function removeBtn(element) {
+    element.remove();
+}
+
+function alertLike(element) {
+    alert("Ninja was liked");
+}
